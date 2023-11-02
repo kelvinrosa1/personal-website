8193167e5467e4041d40b582662127b2ad2eea55
kelvin <kellvin1@icloud.com>
2023-11-02 02:40:31 -0500

    finished my project section

--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React from "react";
+import ProjectCard from "./ProjectCard";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "Puppy Bowl Website",
+    description: "Project 1 description",
+    image: "/Images/projects/1.png",
+    tag: ["All", "Web"],
+    gitUrl: '/',
+    previewUrl: '/',
+  },
+];
 
 export const ProjectSection = () => {
   return (
-    <><h2>My Projects</h2></>
-  )
-}
+    <>
+      <h2>My Projects</h2>
+      <div>
+        {projectsData.map((project) => (
+          <ProjectCard
+            key={project.id}
+            title={project.title}
+            description={project.description}
+            imgUrl={project.image}
+            gitUrl={project.gitUrl}
+            previewUrl={project.previewUrl}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
